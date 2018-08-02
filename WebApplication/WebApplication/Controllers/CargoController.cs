abedec31410bbcec811994a1d8a1d12066038855
hermilocap <hermilocap@hotmail.com>
2018-08-02 00:21:56 -0500

    Aplicacion web de Escuela con modelo MVC

---
+++ WebApplication/WebApplication/Controllers/CargoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class CargoController : Controller
+    {
+        // GET: Cargo
+        EscuelaEntities entidad = new EscuelaEntities();
+        public ActionResult Index()
+        {
+            var listacargo = entidad.cargo;
+
+            return View(listacargo.ToList());
+        }
+        public ActionResult ListaMaestraCargos() {
+            var listacargos = entidad.cargo;
+            return View(listacargos.ToList());
+        }
+        public ActionResult UsuarioCargo(string cargo) {
+            var modelo = from p in entidad.usuario where p.cargo.car_des == cargo select p;
+            return View(modelo.ToList());
+        }
+        public ActionResult ListaUsuariodDescripcion() {
+            var modelo = from p in entidad.usuario
+                         join c in entidad.cargo
+                              on p.car_cod equals c.car_cod
+                         select new ElUsuario
+                         {
+                             codigo = p.usu_cod,
+                             nombre = p.usu_nom,
+                             cargo = c.car_des
+                         };
+           return View(modelo.ToList());
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Controllers/MantenimientoCargoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class MantenimientoCargoController : Controller
+    {
+        private EscuelaEntities db = new EscuelaEntities();
+
+        // GET: MantenimientoCargo
+        public ActionResult Index()
+        {
+            return View(db.cargo.ToList());
+        }
+
+        // GET: MantenimientoCargo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cargo cargo = db.cargo.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // GET: MantenimientoCargo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MantenimientoCargo/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "car_cod,car_des")] cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.cargo.Add(cargo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cargo);
+        }
+
+        // GET: MantenimientoCargo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cargo cargo = db.cargo.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: MantenimientoCargo/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "car_cod,car_des")] cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cargo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cargo);
+        }
+
+        // GET: MantenimientoCargo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cargo cargo = db.cargo.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: MantenimientoCargo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            cargo cargo = db.cargo.Find(id);
+            db.cargo.Remove(cargo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
